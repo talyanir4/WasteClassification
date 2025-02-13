77d8c4c05b0600c1ecbc0f81ab5b6025e758d761
talyanir4 <toolona@gmail.com>
2025-02-13 02:15:12 +0000

    fixed camera code

--- main.py
+++ main.py
@@ -8,87 +8,63 @@ from sklearn.neural_network import MLPClassifier
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 
-
 # constants
 IMG_SIZE = (224, 224)
 IMG_ADDRESS = "https://static.vecteezy.com/system/resources/previews/004/341/571/non_2x/waste-management-eco-friendly-living-2d-web-banner-poster-garbage-separation-man-and-woman-sorting-trash-flat-characters-on-cartoon-background-printable-patches-colorful-web-elements-vector.jpg"
 IMAGE_NAME = "user_image.png"
-CLASS_LABEL = ["cardboard", "glass","metal", "paper", "plastic", "trash"]
+CLASS_LABEL = ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
 CLASS_LABEL.sort()
-
 
 @st.cache_resource
 def get_ConvNeXtXLarge_model():
-
-    # Download the model, valid alpha values [0.25,0.35,0.5,0.75,1]
     base_model = tf.keras.applications.ConvNeXtXLarge(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
-    # Add average pooling to the base
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    model_frozen = Model(inputs=base_model.input,outputs=x)
-
+    model_frozen = Model(inputs=base_model.input, outputs=x)
     return model_frozen
-
 
 @st.cache_resource
 def load_sklearn_models(model_path):
-
     with open(model_path, 'rb') as model_file:
         final_model = pickle.load(model_file)
-
     return final_model
 
-
 def featurization(image_path, model):
-
     img = tf.keras.preprocessing.image.load_img(image_path, target_size=IMG_SIZE)
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_batch = np.expand_dims(img_array, axis=0)
     img_preprocessed = preprocess_input(img_batch)
     predictions = model.predict(img_preprocessed)
-
     return predictions
 
-
-# get the featurization model
+# Load models
 ConvNeXtXLarge_featurized_model = get_ConvNeXtXLarge_model()
-# load ultrasound image
 classification_model = load_sklearn_models("MLP_best_model.pkl")
 
-
-# web app
-
-# title
+# Web app UI
 st.title("Waste Classification")
-# image
-st.image(IMG_ADDRESS, caption = "Waste Classification")
+st.image(IMG_ADDRESS, caption="Waste Classification")
 
-# input image
-st.subheader("Please Upload a waste image")
+st.subheader("Upload or Capture an Image")
 
-# file uploader
-image = st.file_uploader("Please Upload a waste Image", type = ["jpg", "png", "jpeg"], accept_multiple_files = False, help = "Upload an Image")
+# File uploader
+image = st.file_uploader("Upload a Waste Image", type=["jpg", "png", "jpeg"])
 
-img_file_buffer = st.camera_input("Take a picture")
+# Camera input
+camera_image = st.camera_input("Or Take a Photo")
 
-if img_file_buffer is not None:
-    # Convert the image buffer to a PIL Image
-    img = Image.open(img_file_buffer)
+# Process image from either source
+if image or camera_image:
+    user_image = Image.open(image if image else camera_image)
 
-    # Save the image as a JPEG file
-    img.save(IMAGE_NAME, format="JPEG")
-
-    st.success("Image saved as user_image.jpg")
-
-if image or img_file_buffer:
-    user_image = Image.open(image)
-    # save the image to set the path
+    # Save image to use in featurization
     user_image.save(IMAGE_NAME)
-    # set the user image
-    st.image(user_image, caption = "User Uploaded Image")
 
-    #get the features
-    with st.spinner("Processing......."):
+    # Display the user image
+    st.image(user_image, caption="Selected Image")
+
+    # Extract features and classify
+    with st.spinner("Processing..."):
         image_features = featurization(IMAGE_NAME, ConvNeXtXLarge_featurized_model)
         model_predict = classification_model.predict(image_features)
         result_label = CLASS_LABEL[model_predict[0]]
